5dbbd7a72a2391d6bed150a653d915c61d2378a7
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2023-05-24 15:14:38 +0700

    Initial Commit

---
+++ JumpingBird/JumpingBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumpingBirdApp.swift
+//  JumpingBird
+//
+//  Created by Dimas Aristyo Rahadian on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct JumpingBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
